c2cab011fdf05c5cd1b72d4dfad9297ae8f7bad5
Ihor <107474617+IhorOzerov@users.noreply.github.com>
2022-12-30 13:53:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let data = [
+    {
+        photo,
+        title,
+        price,
+        colors,
+        sizes,
+        details
+    }
+]
+
+let burger = document.getElementById('burgerBtn')
+let nav2 = document.getElementById('nav2')
+let headerNav = document.getElementById('burgerMenu')
+
+burger.addEventListener('click', () => {
+
+    nav2.classList.toggle('hidden')
+    headerNav.classList.toggle('header-nav')
+    headerNav.classList.toggle('burger_menu')
+})
+
+
